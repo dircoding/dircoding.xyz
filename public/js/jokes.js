b271a31ef0bd2526ec55e3ba00cd92b8192091b6
Muhammad Haikal <75706383+justClizz@users.noreply.github.com>
2022-04-01 18:05:39 +0700

    Delete public directory

--- public/js/jokes.js
+++
@@ -1,8 +0,0 @@
-// (C) Dircoding On Github
-// MIT LICENSE
-// READ MORE AT https://github.com/dircoding/dircoding.xyz/blob/main/LICENSE
-
-function iya(){
-  document.getElementById("judul").innerHTML = "Here's a cookie for you!";
-  document.getElementById("iya").innerHTML = "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪";
-}
